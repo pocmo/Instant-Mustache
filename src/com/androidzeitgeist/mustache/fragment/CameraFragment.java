ae424dddf05d20687a21b7de0fef38baafb734be
Sebastian Kaspari <s.kaspari@gmail.com>
2012-10-05 18:42:14 +0200

    CameraFragment: Displaying the camera preview.
    Use 4:3 ratio for camera preview and picture size.

--- src/com/androidzeitgeist/mustache/fragment/CameraFragment.java
+++ src/com/androidzeitgeist/mustache/fragment/CameraFragment.java
@@ -1,18 +1,38 @@
 package com.androidzeitgeist.mustache.fragment;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.androidzeitgeist.mustache.listener.CameraFragmentListener;
+import com.androidzeitgeist.mustache.view.CameraPreview;
 
 /**
  * Fragment for displaying the camera preview.
  *
  * @author Sebastian Kaspari <sebastian@androidzeitgeist.com>
  */
-public class CameraFragment extends Fragment {
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
     public static final String TAG = "Mustache/CameraFragment";
 
+    private static final int PICTURE_SIZE_MAX_WIDTH = 1280;
+    private static final int PREVIEW_SIZE_MAX_WIDTH = 640;
+
+    private int cameraId;
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
     private CameraFragmentListener listener;
 
     /**
@@ -29,5 +49,193 @@ public class CameraFragment extends Fragment {
         }
 
         listener = (CameraFragmentListener) activity;
+    }
+
+    /**
+     * On creating view for fragment.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        CameraPreview previewView = new CameraPreview(getActivity());
+
+        previewView.getHolder().addCallback(this);
+
+        return previewView;
+    }
+
+    /**
+     * On fragment getting resumed.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        try {
+            camera = Camera.open(cameraId);
+        } catch (Exception exception) {
+            Log.e(TAG, "Can't open camera with id " + cameraId, exception);
+
+            listener.onCameraError();
+            return;
+        }
+    }
+
+    /**
+     * On fragment getting paused.
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        stopCameraPreview();
+        camera.release();
+    }
+
+    /**
+     * Start the camera preview.
+     */
+    private synchronized void startCameraPreview() {
+        determineDisplayOrientation();
+        setupCamera();
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException exception) {
+            Log.e(TAG, "Can't start camera preview due to IOException", exception);
+
+            listener.onCameraError();
+        }
+    }
+
+    /**
+     * Stop the camera preview.
+     */
+    private synchronized void stopCameraPreview() {
+        try {
+            camera.stopPreview();
+        } catch (Exception exception) {
+            Log.i(TAG, "Exception during stopping camera preview");
+        }
+    }
+
+    /**
+     * Determine the current display orientation and rotate the camera preview
+     * accordingly.
+     */
+    public void determineDisplayOrientation() {
+        CameraInfo cameraInfo = new CameraInfo();
+        Camera.getCameraInfo(cameraId, cameraInfo);
+
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+        int degrees  = 0;
+
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int displayOrientation;
+
+        if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            displayOrientation = (cameraInfo.orientation + degrees) % 360;
+            displayOrientation = (360 - displayOrientation) % 360;
+        } else {
+            displayOrientation = (cameraInfo.orientation - degrees + 360) % 360;
+        }
+
+        camera.setDisplayOrientation(displayOrientation);
+    }
+
+    /**
+     * Setup the camera parameters.
+     */
+    public void setupCamera() {
+        Camera.Parameters parameters = camera.getParameters();
+
+        Size bestPreviewSize = determineBestPreviewSize(parameters);
+        Size bestPictureSize = determineBestPictureSize(parameters);
+
+        parameters.setPreviewSize(bestPreviewSize.width, bestPreviewSize.height);
+        parameters.setPictureSize(bestPictureSize.width, bestPictureSize.height);
+
+        camera.setParameters(parameters);
+    }
+
+    private Size determineBestPreviewSize(Camera.Parameters parameters) {
+        List<Size> sizes = parameters.getSupportedPreviewSizes();
+
+        return determineBestSize(sizes, PREVIEW_SIZE_MAX_WIDTH);
+    }
+
+    private Size determineBestPictureSize(Camera.Parameters parameters) {
+        List<Size> sizes = parameters.getSupportedPictureSizes();
+
+        return determineBestSize(sizes, PICTURE_SIZE_MAX_WIDTH);
+    }
+
+    protected Size determineBestSize(List<Size> sizes, int widthThreshold) {
+        Size bestSize = null;
+
+        for (Size currentSize : sizes) {
+            boolean isDesiredRatio = (currentSize.width / 4) == (currentSize.height / 3);
+            boolean isBetterSize = (bestSize == null || currentSize.width > bestSize.width);
+            boolean isInBounds = currentSize.width <= PICTURE_SIZE_MAX_WIDTH;
+
+            if (isDesiredRatio && isInBounds && isBetterSize) {
+                bestSize = currentSize;
+            }
+        }
+
+        if (bestSize == null) {
+            listener.onCameraError();
+
+            return sizes.get(0);
+        }
+
+        return bestSize;
+    }
+
+    /**
+     * On camera preview surface created.
+     *
+     * @param holder
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        this.surfaceHolder = holder;
+
+        startCameraPreview();
+    }
+
+    /**
+     * On camera preview surface changed.
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // The interface forces us to have this method but we don't need it
+        // up to now.
+    }
+
+    /**
+     * On camera preview surface getting destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // We don't need to handle this case as the fragment takes care of
+        // releasing the camera when needed.
     }
 }
---
+++ src/com/androidzeitgeist/mustache/view/CameraPreview.java
@@ -0,0 +1,44 @@
+package com.androidzeitgeist.mustache.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * {@link SurfaceView} for displaying a squared camera preview.
+ *
+ * @author Sebastian Kaspari <sebastian@androidzeitgeist.com>
+ */
+public class CameraPreview extends SurfaceView {
+    private static final double ASPECT_RATIO = 3.0 / 4.0;
+
+    public CameraPreview(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CameraPreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CameraPreview(Context context) {
+        super(context);
+    }
+
+    /**
+     * Measure the view and its content to determine the measured width and the
+     * measured height.
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+
+        if (width > height * ASPECT_RATIO) {
+             width = (int) (height * ASPECT_RATIO + .5);
+        } else {
+            height = (int) (width / ASPECT_RATIO + .5);
+        }
+
+        setMeasuredDimension(width, height);
+    }
+}
